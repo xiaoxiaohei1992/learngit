c05020fe3d974699c5c7ae43f3cff6b7bd44232f
xiaohei <email@example.com>
2016-09-24 18:52:05 +0800

    new file:   testCPP1-2/test1.cpp 	new file:   testCPP1-2/test2.cpp 	new file:   testCPP3-4/test3.cpp 	new file:   testCPP3-4/test4.cpp

---
+++ testCPP3-4/test4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int i = 1;
+
+int main()
+{
+    int i = 'a';
+    const int &a = i ;
+    i = 3;
+    string s = "aaa\n";
+    int ia[3][4] = {};
+    int *p = ia[0];
+    cout<<(*p + 1)<<endl;
+    return 0;
+}
